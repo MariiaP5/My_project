5a8aa651c4a44a4150a8a5ef6fe07f56cf4b9756
MariiaP5 <mariia_pigusova@mail.ru>
2023-08-24 15:02:47 +0300

    1.0

--- advertisements/admin.py
+++ advertisements/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import Advertisement
+# импортирую класс для подсказок
+from django.db.models.query import QuerySet
 
-# Register your models here.
+
+# admin_class - класс для кастомизации
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'auction', 'updated_date', 'created_date'] # отображение в виде таблице
+    list_filter = ['auction','created_at'] # параметры фильтрации
+    actions = ['make_action_as_false','make_action_as_true'] # добавляю функцию для выбранных записей
+    
+    # создание блоков
+    fieldsets = (
+        ( # блок 1
+            'Общее', # название блока 
+            { 
+                "fields": ('title','description') # поля блока
+            }
+        ),
+        ( # блок 2
+            'Финансы', # название блока 
+            { 
+                "fields": ('price','auction'), # поля блока
+                'classes': ['collapse'], # скрыть/показать блок
+                'description':'блок финансов' # подсказка о блоке
+            }
+        )
+    )
+
+    @admin.action(description='Убрать возможность торга')
+    def make_action_as_false(self, request, queryset:QuerySet):
+        queryset.update(auction = False) # обновить значение auction у выбранных записей на False
+    
+    @admin.action(description='Добавить возможность торга')
+    def make_action_as_true(self, request, queryset:QuerySet):
+        queryset.update(auction = True) # обновить значение auction у выбранных записей на True
+
+
+# подключаю модель
+admin.site.register(Advertisement, AdvertisementAdmin)
--- advertisements/models.py
+++ advertisements/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib import admin
+from django.utils import timezone # для времени
+from django.utils.html import format_html # для создания строки html 
 
 # название, цена, описание, дата создания/обновления, торг
 
@@ -18,9 +21,25 @@ class Advertisement(models.Model): # наследую класс Model для с
     # область для работы с таблицами
     class Meta:
         db_table='advertisements'
-
-
-
+ 
+    @admin.display(description='дата создания')
+    def created_date(self):
+        if self.created_at.date() == timezone.now().date(): # проверяю, что запись была создана сегодня
+            created_time =  self.created_at.time().strftime('%H:%M:%S') # (19:30:15)
+            return format_html(
+                "<span style='color:green; font-weight: bold'>Сегодня в {}</span>",created_time
+            )
+        return self.created_at.strftime('%d.%m.%Y at %H:%M:%S')
+    
+    @admin.display(description='дата обновления')
+    def updated_date(self):
+        if self.updated_at.date() == timezone.now().date():#проверяю что запись была создана сегодня
+            updated_time =  self.updated_at.time().strftime('%H:%M:%S') # 19:30:15
+            return format_html(
+                "<span style='color:green; font-weight: bold'>Сегодня в {}</span>",updated_time
+            )
+        return self.updated_at.strftime('%d.%m.%Y at %H:%M:%S')
+    
 
 # from advertisements.models import Advertisement
 # adv1=Advertisement(title='Машина', description='Машина новая, большая, красная', price='20000000', auction=True) # создала запись
